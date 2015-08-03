6a46fa79aebfbdb1f6d710b22238d5fdd02c21e2
andela-ymakanjuola <yinka.makanjuola@gmail.com>
2015-08-03 18:57:58 +0100

    add intercept column

--- logreg.py
+++ logreg.py
@@ -8,20 +8,18 @@ df = pd.read_json("dataset.json")
 # df.convert_objects(convert_numeric=True)
 
 print df.head()
-# print df.describe()
-# data = df[['cancelled', 'amount', 'cab_service_req', 'is_phone_booking', 'made_on_behalf', 'number_of_rooms',
-#  'special_request_made']]
+print df.describe()
+data = df[['cancelled', 'amount', 'cab_service_req', 'is_phone_booking', 'made_on_behalf', 'number_of_rooms',
+ 'special_request_made']]
+ 
+data.hist()
+pl.show()
+data['intercept'] = 1;
+train_cols = data.columns[1:]
+print train_cols
 
-# print data.head()
-# print data.describe()
-# data.hist()
-# # pl.show()
-# # data['intercept'] = 1;
-# print(data.columns[1:])
-# Index([gre, gpa, prestige_2, prestige_3, prestige_4], dtype=object)
+logit = sm.Logit(data['cancelled'], data[train_cols])
 
-# logit = sm.Logit(data['cancelled'], data[train_cols])
-
-# # fit the model
-# result = logit.fit()
-# print result
+# fit the model
+result = logit.fit()
+print result.summary()
